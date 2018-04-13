efe3905892c80dc08ebe6d42a64364575e33047d
chanlumin <chanlumin@163.com>
2018-04-14 01:45:07 +0800

    热加载 sourcemap html-webpack-plugin clean-webpack-plugin

--- javascript/2_webpack基本使用/src/src/index.js
+++ javascript/2_webpack基本使用/src/src/index.js
@@ -1,3 +1,4 @@
+import printMe from './print'
 class Rectangle {
   // constructor
   constructor(height, width) {
@@ -17,3 +18,22 @@ class Rectangle {
 const square = new Rectangle(10, 10);
 
 console.log(square.area);
+
+/**
+ * 创建一个带有btn的 div
+ * @returns {Element}
+ */
+function component() {
+  let element = document.createElement('div'),
+      btn = document.createElement('button');
+  btn.innerHTML = "Click me"
+  btn.onclick = printMe
+
+  element.appendChild(btn)
+
+  return element
+}
+
+// 添加 element 到body中去
+
+document.body.appendChild(component())
--- javascript/2_webpack基本使用/src/webpack.config.js
+++ javascript/2_webpack基本使用/src/webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    // dist文件改变的话就自动刷新浏览器
+    contentBase: './dist'
+  },
   output: {
-    filename: 'bundle.js',
+    // 此处的name会替换成app和print=> app.bundle.js和 print.bundle.js
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -14,5 +25,13 @@ module.exports = {
         loader: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'HtmlWebpackPlugin'
+    }),
+    // build前会清理dist目录
+    new CleanWebpackPlugin(['dist'])
+
+  ]
 }
